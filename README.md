4b5d08623e3f853bd223eb0506cba6b0927761c4
Alexandr <alex@pop-os.localdomain>
2023-10-20 17:20:46 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# ffmpeg_prikol
+# ffmpeg-to-webrtc
+
+ffmpeg-to-webrtc demonstrates how to send video from ffmpeg to your browser using [pion](https://github.com/pion/webrtc).
+
+## How to run it
+
+### Open example web page
+[jsfiddle.net](https://jsfiddle.net/wjo4e9c7/1/)
+
+### Copy browser's SDP
+In the jsfiddle the top textarea is your browser's SDP, copy that to clipboard.
+
+#### Windows
+1. `cd src`
+1. Paste the SDP into a file `src/SDP.txt`.
+2. Make sure ffmpeg in your PATH and golang is installed.
+3. Run `go run . <ffmpeg command line options> - < SDP.txt`
+4. Note dash after ffmpeg options. It makes ffmpeg to write output to stdout. The app will read h264 stream from ffmpeg stdout.
+5. ffmpeg output format should be h264. Browsers don't support all h264 profiles so it may not always work. Here is an example of format that works: `-pix_fmt yuv420p -c:v libx264 -bsf:v h264_mp4toannexb -b:v 2M -max_delay 0 -bf 0 -f h264`.
+
+### Put SDP from ffmpeg-to-webrtc into your browser
+When you see SDP in base64 format printed it means that SDP is already in clipboard. So you can go to jsfiddle page and paste that into Application SDP text area.
+
+### Hit 'Start Session' in jsfiddle
+A video should start playing in your browser below the input boxes.
+
+## Examples (windows)
+### Share camera stream
+```go run . -rtbufsize 100M -f dshow -i video="PUT_DEVICE_NAME" -pix_fmt yuv420p -c:v libx264 -bsf:v h264_mp4toannexb -b:v 2M -max_delay 0 -bf 0 -f h264 - < SDP```. 
+There is a delay of several seconds. Should be possible to fix it with better ffmpeg configuration.
+
+To check list of devices: `ffmpeg -list_devices true -f dshow -i dummy`.  
+It is possible also to set a resolution and a format, for example `-pixel_format yuyv422 -s 640x480`.
+Possible formats: `ffmpeg -list_options true -f dshow -i video=PUT_DEVICE_NAME`.
+### Share screen or window
+See `.bat` files in src folder
+
+## Linux, macOS
+
+Should work on other operating systems, though I haven't tried.
---
+++ main.go
@@ -0,0 +1,174 @@
+//go:build !js
+// +build !js
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/atotto/clipboard"
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media"
+	"github.com/pion/webrtc/v3/pkg/media/h264reader"
+)
+
+const (
+	h264FrameDuration = time.Millisecond * 33
+)
+
+func main() { //nolint
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if cErr := peerConnection.Close(); cErr != nil {
+			fmt.Printf("cannot close peerConnection: %v\n", cErr)
+		}
+	}()
+
+	iceConnectedCtx, iceConnectedCtxCancel := context.WithCancel(context.Background())
+
+	// Create a video track
+	videoTrack, videoTrackErr := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264}, "video", "pion")
+	if videoTrackErr != nil {
+		panic(videoTrackErr)
+	}
+
+	rtpSender, videoTrackErr := peerConnection.AddTrack(videoTrack)
+	if videoTrackErr != nil {
+		panic(videoTrackErr)
+	}
+
+	// Read incoming RTCP packets
+	// Before these packets are returned they are processed by interceptors. For things
+	// like NACK this needs to be called.
+	go func() {
+		rtcpBuf := make([]byte, 1500)
+		for {
+			if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		fmt.Printf("Input data revieving\n")
+		dataPipe, err := RunCommand("ffmpeg", os.Args[1:]...)
+
+		if err != nil {
+			panic(err)
+		}
+
+		h264, h264Err := h264reader.NewReader(dataPipe)
+		if h264Err != nil {
+			panic(h264Err)
+		}
+
+		// Wait for connection established
+		<-iceConnectedCtx.Done()
+
+		// Send our video file frame at a time. Pace our sending so we send it at the same speed it should be played back as.
+		// This isn't required since the video is timestamped, but we will such much higher loss if we send all at once.
+		//
+		// It is important to use a time.Ticker instead of time.Sleep because
+		// * avoids accumulating skew, just calling time.Sleep didn't compensate for the time spent parsing the data
+		// * works around latency issues with Sleep (see https://github.com/golang/go/issues/44343)
+		spsAndPpsCache := []byte{}
+		ticker := time.NewTicker(h264FrameDuration)
+		for ; true; <-ticker.C {
+			nal, h264Err := h264.NextNAL()
+			if h264Err == io.EOF {
+				fmt.Printf("All video frames parsed and sent")
+				os.Exit(0)
+			}
+			if h264Err != nil {
+				panic(h264Err)
+			}
+
+			nal.Data = append([]byte{0x00, 0x00, 0x00, 0x01}, nal.Data...)
+
+			if nal.UnitType == h264reader.NalUnitTypeSPS || nal.UnitType == h264reader.NalUnitTypePPS {
+				spsAndPpsCache = append(spsAndPpsCache, nal.Data...)
+				continue
+			} else if nal.UnitType == h264reader.NalUnitTypeCodedSliceIdr {
+				nal.Data = append(spsAndPpsCache, nal.Data...)
+				spsAndPpsCache = []byte{}
+			}
+
+			if h264Err = videoTrack.WriteSample(media.Sample{Data: nal.Data, Duration: time.Second}); h264Err != nil {
+				panic(h264Err)
+			}
+		}
+	}()
+
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+		if connectionState == webrtc.ICEConnectionStateConnected {
+			iceConnectedCtxCancel()
+		}
+	})
+
+	// Set the handler for Peer connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		fmt.Printf("Peer Connection State has changed: %s\n", s.String())
+
+		if s == webrtc.PeerConnectionStateFailed {
+			// Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+			// Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+			// Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+			fmt.Println("Peer Connection has gone to failed exiting")
+			os.Exit(0)
+		}
+	})
+
+	// Wait for the offer to be pasted
+	offer := webrtc.SessionDescription{}
+	Decode(MustReadStdin(), &offer)
+
+	// Set the remote SessionDescription
+	if err = peerConnection.SetRemoteDescription(offer); err != nil {
+		panic(err)
+	}
+
+	// Create answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create channel that is blocked until ICE Gathering is complete
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	if err = peerConnection.SetLocalDescription(answer); err != nil {
+		panic(err)
+	}
+
+	// Block until ICE Gathering is complete, disabling trickle ICE
+	// we do this because we only can exchange one signaling message
+	// in a production application you should exchange ICE Candidates via OnICECandidate
+	<-gatherComplete
+
+	// Output the answer in base64 so we can paste it in browser
+	sdp := Encode(*peerConnection.LocalDescription())
+	fmt.Println(sdp)
+	clipboard.WriteAll(sdp)
+
+	// Block forever
+	select {}
+}
